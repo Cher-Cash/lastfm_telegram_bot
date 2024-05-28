2eee3a7908da3b8ae60343c3e8db8e2ff6a1a065
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-05-28 22:09:06 +1000

    add json method safe alive xml method

--- lastfm.py
+++ lastfm.py
@@ -3,7 +3,7 @@ from xml.dom import minidom
 import requests
 
 
-def get_song_from_api(userName, api_key):
+def get_song_from_api(userName, api_key, format_json=False):
     base_url = "http://ws.audioscrobbler.com/2.0/"
 
     # Параметры запроса
@@ -12,13 +12,17 @@ def get_song_from_api(userName, api_key):
         "nowplaying": "true",
         "user": userName,
         "api_key": api_key,
-        "limit": 1
+        "limit": 1,
     }
+    if format_json:
+        params['format'] = 'json'
 
     # Выполнение GET-запроса с параметрами
     response = requests.get(base_url, params=params)
     if response.status_code != 200:
         return None
+    if format_json:
+        return response.json()
     return response.text
 
 
@@ -34,12 +38,25 @@ def process_xml(data):
     }
 
 
-def check_for_new_song(userName, api_key="460cda35be2fbf4f28e8ea7a38580730"):
-    current_track_xml = get_song_from_api(userName, api_key)
-    if not current_track_xml:
+def process_json(document):
+    current_track = document['recenttracks']['track'][0]
+    song_name = current_track['name']
+    song_artist = current_track['artist']['#text']
+
+    return {
+        'name': song_name,
+        'artist': song_artist,
+        'nowplaying': '@attr' in current_track,
+    }
+
+
+def check_for_new_song(userName, api_key="460cda35be2fbf4f28e8ea7a38580730", format_json=True):
+    current_track_data = get_song_from_api(userName, api_key, format_json=format_json)
+    if not current_track_data:
         return {'error': "sometign went wrong"}
-    song_info = process_xml(current_track_xml)
-    return song_info
+    if format_json:
+        return process_json(current_track_data)
+    return process_xml(current_track_data)
 
 
 if __name__ == "__main__":
