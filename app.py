6f0bbaada72f6aab18098db25793684b7a0128fb
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-06-22 16:47:08 +1000

    create base web app

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from lastfm_bot import User
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def user_index():
+    users = User.get_all_users()
+    return render_template('users.html', users=users)
+
+
+@app.route("/user/<int:user_id>")
+def user_view(user_id):
+    user = User.get_user_from_db(user_id)
+    with sqlite3.connect('testdb.sqlite') as connection:
+        cursor = connection.cursor()
+        get_all_song = """
+                    SELECT * 
+                    FROM played
+                    WHERE user_id = ?;
+                    """
+        cursor.execute(get_all_song, (user_id,))
+        all_songs = cursor.fetchall()
+    return render_template('user.html', user=user, songs=all_songs)
+
+
+if __name__ == "__main__":
+    app.run()
